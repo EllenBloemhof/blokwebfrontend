453c199498166129ec8cf77b0cc2186df829eedd
EllenBloemhof <74147677+EllenBloemhof@users.noreply.github.com>
2020-12-16 20:40:40 +0100

    Delete script.js

--- basiswebsite/scripts/script.js
+++
@@ -1,4 +0,0 @@
-var selectElement = (element) => document.querySelector(element);
-
-selectElement('.mobile-menu').addEventListener('click', () => { selectElement('header').classList.toggle('active');
-});
